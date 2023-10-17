d497d4b297bf43218fa77676c630cfa0c7603427
Park JaeHong <pjhrecr@gmail.com>
2023-10-17 23:25:03 +0900

    23.10.17

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>TODO EPPO!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- scoped == 이 콤포넨토 안에서만 적용 됨 -->
+<style scoped>
+  h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin : 2.5rem 0 1.5rem;
+  }
+</style>
